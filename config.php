9c4562d0f5a3604fe538547787fa327f04f22f0b
lewismumbi745 <lewismumbi745@gmail.com>
2026-07-25 15:46:51 +0300

    Refactor database connection to use environment variables
    Updated database connection settings to use environment variables for host, port, database name, username, and password.

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php
-$host = 'localhost';
-$db   = 'price_system';
-$user = 'root';
-$pass = '';
+$host = getenv('mysql.railway.internal');
+$port = getenv('3306');
+$db   = getenv('railway');
+$user = getenv('root');
+$pass = getenv('UmIoFcSAHDnAOJcnDXLPoLhJnCEFqbOR');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 
